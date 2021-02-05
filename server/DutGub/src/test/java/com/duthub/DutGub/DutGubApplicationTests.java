d9f13b987036262f30ce5aa94f08ce807663eb9e
ArtemMatv <46032437+ArtemMatv@users.noreply.github.com>
2021-02-05 15:30:45 +0200

    Server created

---
+++ server/DutGub/src/test/java/com/duthub/DutGub/DutGubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duthub.DutGub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DutGubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
