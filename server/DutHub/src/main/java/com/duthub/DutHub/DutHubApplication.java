78cf436c5637e824d15314b2d1c92316dcb73f7f
ArtemMatv <46032437+ArtemMatv@users.noreply.github.com>
2021-02-23 16:58:04 +0200

    Small fix for FileStorageProperties

--- server/DutHub/src/main/java/com/duthub/DutHub/DutHubApplication.java
+++ server/DutHub/src/main/java/com/duthub/DutHub/DutHubApplication.java
@@ -1,9 +1,14 @@
 package com.duthub.DutHub;
 
+import com.duthub.DutHub.bll.helpers.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+		FileStorageProperties.class
+})
 public class DutHubApplication {
 
 	public static void main(String[] args) {
